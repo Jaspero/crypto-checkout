753ea3f80290d9712f74ae72388f4f3d12975c1c
Filip Lauc <filip.lauc93@gmail.com>
2021-04-18 13:34:23 +0200

    feat: initial setup for gh-pages

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+
+	import {bch, bsv, btc, eth, ltc, xmr} from '@jaspero/crypto-checkout';
+	import {init} from '@jaspero/crypto-checkout/core';
+
+	btc.wallet = 'bc1qk4th7mv0qzfxup5avrecratg24u9e0l56k9x7e';
+	eth.wallet = '0x751721F9754A35B4f86BaE06295406ea335eba46';
+	ltc.wallet = 'LUKqmHBopaX2PS7zndewaEUwWFGZvX3htC';
+	bch.wallet = 'qpanrffk9xpymjetvm8pf5pxx95zp5hc7sld6qzyrt';
+	bsv.wallet = '1PvPkR8PCpPTq7bK7mQgBwwfX4KhW2eUGUq';
+	xmr.wallet = '1PvPkR8PCpPTq7bK7mQgBwwfX4KhW2eUGU';
+
+	init([btc, eth, ltc, bch, bsv, xmr]);
+
+	let amount = 1;
+
+	function openCheckout() {
+		window.jpCrypto.open({value: amount, message: 'crypto-checkout-demo'})
+	}
+</script>
+
+<main>
+	<input type="number" placeholder="Amount" bind:value={amount}>
+
+	<button on:click={openCheckout}>Checkout</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+	}
+</style>
