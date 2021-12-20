6077ea3914af5ccbb84a3a9a4847565b0d8eb82c
Filip Lauc <filip.lauc93@gmail.com>
2021-12-20 00:50:46 +0000

    feat: added caching for coincap

--- src/services/coincap.service.ts
+++ src/services/coincap.service.ts
@@ -3,12 +3,33 @@ import {CryptoService} from '../types/crypto.service';
 export class CoincapService extends CryptoService {
 
   url = 'https://api.coincap.io/v2/';
+  cacheDuration = 5 * 60 * 1000;
+  cache: {
+    [key: string]: {
+      timeStamp: number;
+      price: number;
+    }
+  } = {};
 
   async getPrice(value, coin) {
+
+    if (this.cache[coin]) {
+      if (this.cache[coin].timeStamp > (Date.now() - this.cacheDuration)) {
+        return value / this.cache[coin].price;
+      } else {
+        delete this.cache[coin];
+      }
+    }
+
     let res: any = await fetch(
       this.url + 'assets/' + coin
     );
     res = await res.json();
+
+    this.cache[coin] = {
+      timeStamp: Date.now(),
+      price: res.data.priceUsd
+    };
 
     return value / res.data.priceUsd;
   }
